1fae4fa66b832e9a339ba0581be5ef63f9ef3785
elmotahajar <elmotahajar@gmail.com>
2026-03-22 13:45:59 +0100

    Revert "Feature/2 divide1"

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,17 +1,6 @@
-import pytest
-from src.calculator import add, subtract, divide
-
+from src.calculator import add, subtract
 def test_add():
     assert add(2, 3) == 5
-
 def test_subtract():
     assert subtract(5, 2) == 3
-
-def test_divide():
-    assert divide(10, 2) == 5.0
-    assert divide(7, 2) == 3.5
-
-# Cas limite
-def test_divide_by_zero():
-    with pytest.raises(ValueError, match="Cannot divide by zero"):
-        divide(5, 0)
+    
